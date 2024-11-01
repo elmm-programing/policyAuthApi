3bb58e11dcd8bb64dcb04aa9f00de72795498187
edmejia <edmejia@hacienda.gov.do>
2024-11-01 13:40:56 -0400

    Policy auth endpoint handler search all the resource of a user base on the role

--- internal/models/resource.go
+++ internal/models/resource.go
@@ -5,6 +5,14 @@ type Resource struct {
 	ResourceName string `json:"resource_name"`
 }
 
+type ResourceWithRoleRelation struct {
+	ResourceRoleId   int    `json:"resource_role_id"`
+	ResourceID   int    `json:"resource_id"`
+	ResourceName string `json:"resource_name"`
+  Permissions []string `json:"resource_permissions"`
+}
+
+
 //Generate a json example with the struct Resource
 // {
 //  "resource_id": 1,
--- internal/server/handlers/authorization/policy_handler.go
+++ internal/server/handlers/authorization/policy_handler.go
@@ -4,7 +4,9 @@ import (
 	"database/sql"
 	"strings"
 
+	models "policyAuth/internal/models"
 	"github.com/gofiber/fiber/v2"
+	log "github.com/gofiber/fiber/v2/log"
 )
 
 type ResourceDetailsHandler struct {
@@ -25,23 +27,22 @@ func (h *ResourceDetailsHandler) userExists(username string) (bool, int, error) 
 
 func (h *ResourceDetailsHandler) GetRolesAndPermissionsForResource(c *fiber.Ctx) error {
 	username := c.Params("username")
-  userExists, userID, err := h.userExists(username)
-
+	userExists, userID, err := h.userExists(username)
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
 	}
 	if !userExists {
 		return c.Status(fiber.StatusBadRequest).SendString("User does not exist")
 	}
-  userRolesRows, err := h.DB.Query(`
+	userRolesRows, err := h.DB.Query(`
 		SELECT r.role_name
 		FROM pds_roles r
 		JOIN pds_user_roles ur ON r.role_id = ur.role_id
 		WHERE ur.user_id = $1`, userID)
-if err != nil {
+	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
 	}
-  var userRoles []string
+	var userRoles []string
 	for userRolesRows.Next() {
 		var roleName string
 		if err := userRolesRows.Scan(&roleName); err != nil {
@@ -49,11 +50,11 @@ if err != nil {
 		}
 		userRoles = append(userRoles, roleName)
 	}
-if len(userRoles) == 0 {
+	if len(userRoles) == 0 {
 		return c.Status(fiber.StatusBadRequest).SendString("User has no roles")
 	}
-query := `
-		SELECT DISTINCT res.resource_name
+	query := `
+		SELECT DISTINCT res.resource_id , res.resource_name,rr.id
 		FROM pds_resources res
 		JOIN pds_resource_role rr ON res.resource_id = rr.resource_id
 		JOIN pds_roles r ON rr.role_id = r.role_id
@@ -64,26 +65,50 @@ query := `
 	}
 	defer resourceRows.Close()
 
-	var resources []string
+	var resources []models.ResourceWithRoleRelation
 	for resourceRows.Next() {
-		var resourceName string
-		if err := resourceRows.Scan(&resourceName); err != nil {
+  var resource models.ResourceWithRoleRelation
+		if err := resourceRows.Scan(&resource.ResourceID,&resource.ResourceName,&resource.ResourceRoleId); err != nil {
 			return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
 		}
-		resources = append(resources, resourceName)
+		resources = append(resources, resource)
 	}
-
-
-
-	// Create the response structure
-	response := map[string]interface{}{
+	query2 := `
+		SELECT DISTINCT pp.permission_name
+		FROM pds_permissions pp 
+		JOIN pds_resource_permission prp ON prp.permission_id = pp.permission_id
+		JOIN pds_role_resource_permissions rrp ON rrp.permission_id = pp.permission_id
+		where prp.resource_id = $1 and rrp.resource_role_id = $2`
+  for i, v := range resources {
+    
+	permissionsRows, err := h.DB.Query(query2, v.ResourceID,v.ResourceRoleId)
+    if err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
 	}
-  for _,resource := range resources {
-    response[resource] = map[string]interface{}{
-      "roles": userRoles,
-    }
+	defer permissionsRows.Close()
+
+	for permissionsRows.Next() {
+		var permissionsName string
+		if err := permissionsRows.Scan(&permissionsName); err != nil {
+			return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+		}
+      resources[i].Permissions = append(resources[i].Permissions, permissionsName)
+	}
 
   }
+
+	// log.Print("{"+strings.Join(userRoles, ",")+"}")
+	log.Info(resources)
+	log.Info("{" + strings.Join(userRoles, ",") + "}")
+	
+	// Create the response structure
+	response := map[string]interface{}{}
+	for _, resource := range resources {
+		response[resource.ResourceName] = map[string]interface{}{
+			"roles": userRoles,
+      "permissions": resource.Permissions,
+		}
+	}
 
 	return c.JSON(response)
 }
