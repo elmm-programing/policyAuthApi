f688268b0dd50f1a0bba5d2931fef8726d6a1d1f
ELMM <elmm.programing@gmail.com>
2024-11-03 18:08:31 -0500

    Change resource_permission handler to use resource_permission_service and resource_permission model added id to return in get

--- internal/models/relations/resource_permission.go
+++ internal/models/relations/resource_permission.go
@@ -1,6 +1,7 @@
 package relations
 
 type ResourcePermission struct {
+  Id int `json:"id"`
 	ResourceID   int `json:"resource_id"`
 	ResourceName   string `json:"resource_name"`
 	PermissionID int `json:"permission_id"`
--- internal/server/handlers/relations/resource_permissions_handler.go
+++ internal/server/handlers/relations/resource_permissions_handler.go
@@ -1,69 +1,51 @@
 package relations
 
+
 import (
-	"database/sql"
+    "database/sql"
+    "policyAuth/internal/models/relations"
+    services "policyAuth/internal/services/relations"
 
-	"policyAuth/internal/models/relations"
-
-	"github.com/gofiber/fiber/v2"
+    "github.com/gofiber/fiber/v2"
 )
 
 // ResourcePermissionHandler handles resource-permission-related requests
 type ResourcePermissionHandler struct {
-	DB *sql.DB
+    ResourcePermissionService services.ResourcePermissionService
+}
+
+func NewResourcePermissionHandler(DB *sql.DB) *ResourcePermissionHandler {
+    return &ResourcePermissionHandler{ResourcePermissionService: *services.NewResourcePermissionService(DB)}
 }
 
 func (h *ResourcePermissionHandler) GetResourcePermissions(c *fiber.Ctx) error {
-	rows, err := h.DB.Query(`
-SELECT 
-    rp.resource_id, 
-    r.resource_name, 
-    rp.permission_id, 
-    p.permission_name
-FROM 
-    pds_resource_permission rp
-JOIN 
-    pds_resources r ON rp.resource_id = r.resource_id
-JOIN 
-    pds_permissions p ON rp.permission_id = p.permission_id;
-    `)
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
-	}
-	defer rows.Close()
+    resourcePermissions, err := h.ResourcePermissionService.GetResourcePermissions()
+    if err != nil {
+        return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+    }
 
-	var resourcePermissions []relations.ResourcePermission
-	for rows.Next() {
-		var resourcePermission relations.ResourcePermission
-		if err := rows.Scan(&resourcePermission.ResourceID,&resourcePermission.ResourceName, &resourcePermission.PermissionID,&resourcePermission.PermissionName); err != nil {
-			return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
-		}
-		resourcePermissions = append(resourcePermissions, resourcePermission)
-	}
-
-	return c.JSON(resourcePermissions)
+    return c.JSON(resourcePermissions)
 }
 
 func (h *ResourcePermissionHandler) CreateResourcePermission(c *fiber.Ctx) error {
-	var resourcePermission relations.ResourcePermission
-	if err := c.BodyParser(&resourcePermission); err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
-	}
+    var resourcePermission relations.ResourcePermission
+    if err := c.BodyParser(&resourcePermission); err != nil {
+        return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+    }
 
-	_, err := h.DB.Exec("INSERT INTO pds_resource_permission (resource_id, permission_id) VALUES ($1, $2)", resourcePermission.ResourceID, resourcePermission.PermissionID)
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
-	}
+    if err := h.ResourcePermissionService.CreateResourcePermission(resourcePermission); err != nil {
+        return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+    }
 
-	return c.JSON(resourcePermission)
+    return c.JSON(resourcePermission)
 }
 
 func (h *ResourcePermissionHandler) DeleteResourcePermission(c *fiber.Ctx) error {
-	id := c.Params("id")
-	_, err := h.DB.Exec("DELETE FROM pds_resource_permission WHERE id=$1", id)
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
-	}
+    id := c.Params("id")
+    if err := h.ResourcePermissionService.DeleteResourcePermission(id); err != nil {
+        return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+    }
 
-	return c.SendStatus(fiber.StatusNoContent)
+    return c.SendStatus(fiber.StatusNoContent)
 }
+
