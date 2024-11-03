25efa25fcf009611da0470d3da30cdc548ab6608
ELMM <elmm.programing@gmail.com>
2024-11-03 18:03:11 -0500

    More data inserted to test

--- internal/database/database.go
+++ internal/database/database.go
@@ -68,19 +68,145 @@ CREATE TABLE IF NOT EXISTS pds_role_resource_permissions (
     permission_id INT REFERENCES pds_permissions(permission_id)
 );
 
-  INSERT INTO pds_roles (role_name) VALUES
-		('admin'),
-		('user')
-	ON CONFLICT (role_name) DO NOTHING;
-	`
+INSERT INTO pds_users (username,"password")
+	VALUES 
+	('edwin','edwin'),
+	('anonimo','anonimo'),
+	('admin','admin')
+ON CONFLICT (username) DO NOTHING;
+
+INSERT INTO pds_roles (role_name)
+	VALUES 
+	('admin'),
+	('anonimo'),
+	('user'),
+	('analista')
+ON CONFLICT (role_name) DO NOTHING;
+
+INSERT INTO pds_resources (resource_name)
+	VALUES 
+	('/home'),
+	('/analistas'),
+	('/normalUser'),
+	('/anonimo'),
+	('/ue'),
+	('/clasificadores')
+ON CONFLICT (resource_name) DO NOTHING;
+
+INSERT INTO pds_permissions (permission_name)
+	VALUES 
+	('get'),
+	('post'),
+	('update'),
+	('delete')
+ON CONFLICT (permission_name) DO NOTHING;	
+
+INSERT INTO pds_user_roles (user_id,role_id)
+	VALUES 
+	(3,1),
+	(2,2),
+	(1,4),
+	(1,3)
+ON CONFLICT (user_id,role_id) DO NOTHING;
+
+
+INSERT INTO public.pds_resource_role (id,resource_id,role_id)
+	VALUES (1,1,1),
+	(2,1,2),
+	(3,1,3),
+	(4,1,4),
+	(5,2,1),
+	(6,2,4),
+	(7,3,3),
+	(8,4,2),
+  (9,5,3),
+  (10,5,4),
+  (11,6,3),
+  (12,6,4) ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.pds_resource_permission (id,resource_id,permission_id)
+	VALUES 
+	(1,1,1), 
+	(2,1,2),
+	(3,1,3),
+	(4,1,4),
+	(5,2,1), 
+	(6,2,2),
+	(7,2,3),
+	(8,2,4),
+	(9,3,1), 
+	(10,3,2),
+	(11,3,3),
+	(12,3,4),
+	(13,4,1), 
+	(14,4,2),
+	(15,4,3),
+	(16,4,4),
+  (17,5,1), 
+	(18,5,2),
+	(19,5,3),
+	(20,5,4),
+  (21,6,1), 
+	(22,6,2),
+	(23,6,3),
+	(24,6,4)
+
+	ON CONFLICT (id) DO NOTHING;
+
+
+INSERT INTO pds_role_resource_permissions (id,resource_role_id,permission_id)
+	VALUES 
+	(1,1,1), 
+	(2,1,2),
+	(3,1,3),
+	(4,1,4),
+	(5,2,1), 
+	(6,2,2),
+	(7,2,3),
+	(8,2,4),
+	(9,3,1), 
+	(10,3,2),
+	(11,3,3),
+	(12,3,4),
+	(13,4,1), 
+	(14,4,2),
+	(15,4,3),
+	(16,4,4),
+	(17,5,1), 
+	(18,5,2),
+	(19,5,3),
+	(20,5,4),
+	(21,6,1), 
+	(22,6,2),
+	(23,6,3),
+	(24,6,4),
+	(25,7,1), 
+	(26,7,2),
+	(27,7,3),
+	(28,7,4),
+	(29,8,1), 
+	(30,8,2),
+	(31,8,3),
+	(32,8,4),
+  (33,9,1),
+  (34,9,2),
+  (35,10,3),
+  (36,10,4),
+  (37,11,1),
+  (38,11,2),
+  (39,12,3),
+  (40,12,4)
+	ON CONFLICT (id) DO NOTHING;
+
+
+
+`
 
 	_, err := db.Exec(createTableQuery)
 	if err != nil {
 		log.Fatalf("Failed to create table: %v", err)
 	}
-
 }
-
 
 // Service represents a service that interacts with a database.
 type Service interface {
@@ -100,27 +226,27 @@ type DatabaseService struct {
 	Port     string
 	Host     string
 	Schema   string
-  Instance *sql.DB
+	Instance *sql.DB
 }
 
 func New() *DatabaseService {
-  dbServices := &DatabaseService{
-  Database: os.Getenv("DB_DATABASE"),
-  Password: os.Getenv("DB_PASSWORD"),
-    Username: os.Getenv("DB_USERNAME"),
-    Port:     os.Getenv("DB_PORT"),
-    Host:     os.Getenv("DB_HOST"),
-    Schema:   os.Getenv("DB_SCHEMA"),
-  }
+	dbServices := &DatabaseService{
+		Database: os.Getenv("DB_DATABASE"),
+		Password: os.Getenv("DB_PASSWORD"),
+		Username: os.Getenv("DB_USERNAME"),
+		Port:     os.Getenv("DB_PORT"),
+		Host:     os.Getenv("DB_HOST"),
+		Schema:   os.Getenv("DB_SCHEMA"),
+	}
 	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable&search_path=%s", dbServices.Username, dbServices.Password, dbServices.Host, dbServices.Port, dbServices.Database, dbServices.Schema)
-  fmt.Println("The connection string is ", connStr)
+	fmt.Println("The connection string is ", connStr)
 
 	db, err := sql.Open("pgx", connStr)
 	if err != nil {
 		log.Fatal(err)
 	}
-  dbServices.Instance = db
-	
+	dbServices.Instance = db
+
 	return dbServices
 }
 
